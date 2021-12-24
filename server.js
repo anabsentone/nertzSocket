097139c8df727db6c39aabd27b49d8da0dfc0bfd
anabsentone <anabsentone@gmail.com>
2021-12-24 16:15:36 -0500

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,21 +16,51 @@ const botName = 'NertzBot';
 
 // Run when a client connects
 io.on('connection', socket => {
-    // socket.emit() broadcasts to that user
-    socket.emit('message', formatMessage(botName, 'Welcome to nertz'));
+    socket.on('joinRoom', ({username, room}) => {
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
 
-    // socket.broadcast.emit() broadcasts to all clients except that user
-    socket.broadcast.emit('message', formatMessage(botName, 'A new user connected'));
-    
-    // Run when a client disconnects
-    socket.on('disconnect', () => {
-        // io.emit() broadcasts to all users
-        io.emit('message', formatMessage(botName, 'A user has disconnected'));
+        // socket.emit() broadcasts to that user
+        socket.emit('message', formatMessage(botName, 'Welcome to ' + room + '!'));
+
+        // socket.broadcast.emit() broadcasts to all clients in room except that user
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has connected`));
+
+        // Send users and room info
+        io.to(user.room).emit('roomusers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
+    });
+
+    // Listen for team change
+    socket.on('changeTeam', team => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('teamChange', {
+            user,
+            team
+        });
     });
 
     // Listen for chat message
     socket.on('chatMessage', msg => {
-        io.emit('message', formatMessage('USER', msg));
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+    });
+
+    // Run when a client disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+        if (user) {
+            // io.emit() broadcasts to all users
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has disconnected`));
+
+            // Send users and room info
+            io.to(user.room).emit('roomusers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+        }
     });
 });
 
